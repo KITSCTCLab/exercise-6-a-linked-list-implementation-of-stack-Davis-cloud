14199748d4f7f0177a3fab1897ce43a0bd7dceb8
URK21CS2009 Davis Alex <109649381+Davis-cloud@users.noreply.github.com>
2022-10-11 21:33:10 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -33,17 +33,3 @@ class Stack:
       temp = temp.next
     print("None")
 
-
-# Do not change the following code
-stack = Stack()
-operations = []
-for specific_operation in input().split(','):
-    operations.append(specific_operation.strip())
-input_data = input()
-data = input_data.split(',')
-for i in range(len(operations)):
-  if operations[i] == "push":
-    stack.push(int(data[i]))
-  elif operations[i] == "pop":
-    stack.pop()
-
